1a4b75215a6311cec5f908dd9c2a9785d85b7254
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-06-20 20:23:14 +0300

    galanin-pc-css : Updated SingIn, SingUp components

--- src/pages/SingIn/SingIn.jsx
+++ src/pages/SingIn/SingIn.jsx
@@ -1,25 +1,28 @@
-import style from "./SignIn.module.css"
-import GeneralFrame from "../GeneralFrame/GeneralFrame";
-import {Link} from "react-router-dom";
+import style from './SignIn.module.css';
+import { Link } from 'react-router-dom';
 
 function SingIn() {
-    return (
-        <>
-            <GeneralFrame/>
-                <div className={style.window}>
-                    <main className={style.main}>
-                        <h2 className={style.h2}>Sign in</h2>
-                        <form action="">
-                            <input type="email" className={style.input} placeholder={"Email"}/>
-                            <input type="password" className={style.input} placeholder={"Password"}/>
-                            <Link to={"/sing-up"} className={style.Link}>You haven’t account?</Link>
-                            <button className={style.button}>Submit</button>
-                        </form>
-                    </main>
-                </div>
-
-        </>
-    )
+  return (
+    <>
+      <div className={style.window}>
+        <main className={style.main}>
+          <h2 className={style.h2}>Sign in</h2>
+          <form action="">
+            <input type="email" className={style.input} placeholder={'Email'} />
+            <input
+              type="password"
+              className={style.input}
+              placeholder={'Password'}
+            />
+            <Link to={'/sing-up'} className={style.Link}>
+              You haven’t account?
+            </Link>
+            <button className={style.button}>Submit</button>
+          </form>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default SingIn;

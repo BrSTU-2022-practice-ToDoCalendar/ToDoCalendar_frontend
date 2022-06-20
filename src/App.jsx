e1d1bd27b71ecab34a6f5b9fd8a47bec2b1d114f
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-06-20 20:10:25 +0300

    galanin-pc-css : Added Home component

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,17 @@ import './App.css';
 import SingIn from './pages/SingIn/SingIn';
 import SingUp from './pages/SingUp/SingUp';
 import ViewFrame from './pages/ViewFrame/VIewFrame';
-import GeneralFrame from './pages/GeneralFrame/GeneralFrame';
+import Home from './pages/Home/Home';
 import EditFrame from './pages/EditFrame/EditFrame';
 import Error404 from './pages/Error404/Error404';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SingIn />} />
+      <Route path="/" element={<Home />} />
       <Route path="/sing-in" element={<SingIn />} />
       <Route path="/sing-up" element={<SingUp />} />
       <Route path="/view-frame" element={<ViewFrame />} />
-      <Route path="/general-frame" element={<GeneralFrame />} />
       <Route path="/edit-frame" element={<EditFrame />} />
       <Route path="*" element={<Error404 />} />
     </Routes>
